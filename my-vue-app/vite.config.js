cb4119bbf17fdc50178b4b4225f550726f160d75
liu-shuai1234 <2084926541@qq.com>
2023-05-13 16:41:36 +0800

    'element'

--- my-vue-app/vite.config.js
+++ my-vue-app/vite.config.js
@@ -1,7 +1,29 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
+import * as path from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    //配置element ui
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()],
+    }),
+  ],
+  //配置vue3 @路径
+  resolve: {
+    alias: [
+      {
+        find: '@',
+        replacement: path.resolve('./src')
+      }
+    ]
+  },
 })
